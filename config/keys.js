2b209981410e8f4d1233b7502397110dc942d4b1
nerodroid <nerodroid.dev@gmail.com>
2020-10-23 11:31:07 +0530

    added sample routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : "mongodb+srv://nerodroid:0714076576@cluster0.w54fe.mongodb.net/leave_form_db?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = express();
+
+//routes
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+
+
+
+
+
+//DN config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose.connect(db)
+.then(()=> console.log("MongoDB coonnected"))
+.catch(err=> console.log(err));
+
+//use Router
+app.use('/api/users', users);
+app.use('/api/profile',profile);
+
+
+
+
 
 app.get('/',(req,res) => res.send("hello"));
 
